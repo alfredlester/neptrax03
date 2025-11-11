2edc1d108b134c0ac7baece6d7e12592d534c65d
alfredlester <alfredlester657@gmail.com>
2025-11-11 12:13:52 +0000

    Updated MissionSection.tsx

--- src/components/MissionSection.tsx
+++ src/components/MissionSection.tsx
@@ -112,7 +112,7 @@ export default function MissionSection() {
                 <img
                   src="/portfolio/jeton.png"
                   alt="Jeton Project"
-                  className="w-full h-[500px] object-cover"
+                  className="w-full h-[550px] object-cover"
                 />
               </div>
             </div>
